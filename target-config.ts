e6943e90a586fd7a3862e3a7f66e72d4cd7c7f03
CluisRange <cluisrange@gmail.com>
2025-01-30 00:09:51 +0300

    2.0

--- target-config.ts
+++ target-config.ts
@@ -1,2 +1,2 @@
-export const api_proxy_addr = "http://192.168.253.1:8000"
-export const img_proxy_addr = "http://192.168.253.1:9000"
+export const api_proxy_addr = "http://172.20.10.2:8000"
+export const img_proxy_addr = "http://172.20.10.2:9000"
